71a4f9b496f7f43d9c01ed78c2c728e49e63f50a
kanstantsinkamazennikau <k.kamazennikau@itechart-group.com>
2023-07-25 21:18:10 +0300

    removed unnecessary osts with tag page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { Post } from "@prisma/client";
 import NoPosts from "@/components/NoPosts";
 import PostCard from "@/components/PostCard";
 
+interface SearchParams {
+  tag?: string;
+}
+
 async function getAllPosts(): Promise<Post[]> {
   return (
     await fetch("http://localhost:3000/api/posts", {
@@ -10,15 +14,32 @@ async function getAllPosts(): Promise<Post[]> {
   ).json();
 }
 
-export default async function Home() {
-  const usersPosts = await getAllPosts();
+async function getPostsByTag(tag: string): Promise<Post[]> {
+  return (
+    await fetch(`http://localhost:3000/api/posts?tag=${tag}`, {
+      cache: "no-cache",
+    })
+  ).json();
+}
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const tag = searchParams.tag;
+  let posts;
+
+  if (!tag) {
+    posts = await getAllPosts();
+  } else {
+    posts = await getPostsByTag(tag);
+  }
 
   return (
-    <main className="masonry-3">
-      {!!usersPosts.length ? (
-        usersPosts.map((post, index) => (
-          <PostCard key={post.id} post={post} index={index} />
-        ))
+    <main className="masonry">
+      {!!posts.length ? (
+        posts.map((post) => <PostCard key={post.id} post={post} />)
       ) : (
         <NoPosts />
       )}
--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -3,20 +3,19 @@ import Link from "next/link";
 
 interface PostCardProps {
   post: Post;
-  index: number;
 }
 
-export default function PostCard({ post, index }: PostCardProps) {
+export default function PostCard({ post }: PostCardProps) {
   const { title, content, tags } = post;
   return (
     <div className="border-gray-800 border-2 rounded-lg p-1 item">
       <div className="font-semibold capitalize text-lg text-center">
-        {title + index}
+        {title}
       </div>
       <div className="max-h-60 overflow-hidden">{content}</div>
       {tags.map((tag) => (
         <Link
-          href={{ pathname: "/posts", query: { tag } }}
+          href={{ pathname: "/", query: { tag } }}
           key={tag}
           className="mr-1 text-blue-500 cursor-pointer underline hover:text-blue-700"
         >
